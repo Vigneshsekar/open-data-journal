a39f81656e43151144fb28892d4df7976a888e92
vigneshsekar <svignesh0308@gmail.com>
2017-06-19 23:19:30 +0530

    Page controller test case

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Jod.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "The Journal of Open Data"
   end
 end
