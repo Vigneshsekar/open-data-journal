ca563dbd1acfff891e720f29f5d646f327042923
vigneshsekar <svignesh0308@gmail.com>
2017-06-27 08:14:12 +0530

    Case when no user logged in. Temporary fix.

--- web/auth/role_checker.ex
+++ web/auth/role_checker.ex
@@ -3,6 +3,10 @@ defmodule Jod.RoleChecker do
   alias Jod.Role
 
   def is_admin?(user) do
-    (role = Repo.get(Role, user.role_id)) && role.admin
+    if user do
+      (role = Repo.get(Role, user.role_id)) && role.admin
+    else
+      false
+    end
   end
 end
