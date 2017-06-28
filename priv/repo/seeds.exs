e6cc2bebe2d5420b3fa6878374f56fcc445bcd70
vigneshsekar <svignesh0308@gmail.com>
2017-06-28 16:25:42 +0530

    Seed users and role for Heroku deployment

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Jod.Repo
+alias Jod.Role
+alias Jod.User
+role = %Role{}
+  |> Role.changeset(%{name: "Admin Role", admin: true})
+  |> Repo.insert!
+admin = %User{}
+  |> User.changeset(%{username: "admin", email: "admin@test.com", password: "qwerty", password_confirmation: "test", role_id: 1, first_name: "admin_test"})
+  |> Repo.insert!
