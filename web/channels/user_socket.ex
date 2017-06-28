87e88dadac9a405c77be57738835d84c9910e478
vigneshsekar <svignesh0308@gmail.com>
2017-06-28 15:41:56 +0530

    Heroku deployment

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule Jod.UserSocket do
   channel "comments:*", Jod.CommentChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
